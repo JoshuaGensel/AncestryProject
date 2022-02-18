cabf70f8f87317b35edd28a3e9c183671dce5239
Leon Joshua Gensel <joshua.gensel@icloud.com>
2022-02-18 02:43:15 +0100

    new variable names + .samples removing

--- src/tree_parsing/tree_parsing.py
+++ src/tree_parsing/tree_parsing.py
@@ -40,71 +40,71 @@ def haplotypeCalc(path):
     inputFileName = tail
     #Importing and simplifying tree sequence
 
-    treeseq_raw = pyslim.load(path)
-    all_nodes = treeseq_raw.samples()
+    ts_raw = pyslim.load(path)
+    all_nodes = ts_raw.samples()
     nodes_F = []
     nodes_M = []
 
     for u in all_nodes:
-        if treeseq_raw.mutation_at(u,0) != -1 and treeseq_raw.individual(treeseq_raw.node(u).individual).metadata["sex"] == pyslim.INDIVIDUAL_TYPE_FEMALE:
+        if ts_raw.mutation_at(u,0) != -1 and ts_raw.individual(ts_raw.node(u).individual).metadata["sex"] == pyslim.INDIVIDUAL_TYPE_FEMALE:
             nodes_F.append(u)
-        if treeseq_raw.mutation_at(u,916567) != -1 and treeseq_raw.individual(treeseq_raw.node(u).individual).metadata["sex"] == pyslim.INDIVIDUAL_TYPE_MALE:
+        if ts_raw.mutation_at(u,916567) != -1 and ts_raw.individual(ts_raw.node(u).individual).metadata["sex"] == pyslim.INDIVIDUAL_TYPE_MALE:
             nodes_M.append(u)
         else:
             pass
         
     
-    treeseq_mtDNA = treeseq_raw.simplify(samples=nodes_F)
-    treeseq_YChrom = treeseq_raw.simplify(samples=nodes_M)
+    ts_M_raw = ts_raw.simplify(samples=nodes_F)
+    ts_Y_raw = ts_raw.simplify(samples=nodes_M)
 
-    if(treeseq_mtDNA.num_trees != 1): raise ValueError("more than one tree!")
-    if(treeseq_YChrom.num_trees != 1): raise ValueError("more than one tree!")
+    if(ts_M_raw.num_trees != 1): raise ValueError("more than one tree!")
+    if(ts_Y_raw.num_trees != 1): raise ValueError("more than one tree!")
 
-    if(treeseq_mtDNA.first().num_roots > 1): raise ValueError("more than one root!")
-    if(treeseq_YChrom.first().num_roots > 1): raise ValueError("more than one root!")
+    if(ts_M_raw.first().num_roots > 1): raise ValueError("more than one root!")
+    if(ts_Y_raw.first().num_roots > 1): raise ValueError("more than one root!")
 
     nodes_F_p1 = []
     nodes_F_p2 = []
     nodes_F_p3 = []
-    for i in treeseq_mtDNA.individuals_alive_at(0):
-        if treeseq_mtDNA.individual(i).metadata["subpopulation"] == 1:
-            nodes_F_p1.extend(treeseq_mtDNA.individual(i).nodes)
-        elif treeseq_mtDNA.individual(i).metadata["subpopulation"] == 2:
-            nodes_F_p2.extend(treeseq_mtDNA.individual(i).nodes)
-        elif treeseq_mtDNA.individual(i).metadata["subpopulation"] == 3:
-            nodes_F_p3.extend(treeseq_mtDNA.individual(i).nodes)
+    for i in ts_M_raw.individuals_alive_at(0):
+        if ts_M_raw.individual(i).metadata["subpopulation"] == 1:
+            nodes_F_p1.extend(ts_M_raw.individual(i).nodes)
+        elif ts_M_raw.individual(i).metadata["subpopulation"] == 2:
+            nodes_F_p2.extend(ts_M_raw.individual(i).nodes)
+        elif ts_M_raw.individual(i).metadata["subpopulation"] == 3:
+            nodes_F_p3.extend(ts_M_raw.individual(i).nodes)
         else:
             raise ValueError("Individual without population!")
     keep_nodes_F = np.concatenate((np.random.choice(nodes_F_p1, nSamples, replace=False), 
                                    np.random.choice(nodes_F_p2, nSamples, replace=False), 
                                    np.random.choice(nodes_F_p3, nSamples, replace=False)))
-    sts_mtDNA = treeseq_mtDNA.simplify(keep_nodes_F)
+    sts_M = ts_M_raw.simplify(keep_nodes_F)
 
     nodes_M_p1 = []
     nodes_M_p2 = []
     nodes_M_p3 = []
-    for i in treeseq_YChrom.individuals_alive_at(0):
-        if treeseq_YChrom.individual(i).metadata["subpopulation"] == 1:
-            nodes_M_p1.extend(treeseq_YChrom.individual(i).nodes)
-        elif treeseq_YChrom.individual(i).metadata["subpopulation"] == 2:
-            nodes_M_p2.extend(treeseq_YChrom.individual(i).nodes)
-        elif treeseq_YChrom.individual(i).metadata["subpopulation"] == 3:
-            nodes_M_p3.extend(treeseq_YChrom.individual(i).nodes)
+    for i in ts_Y_raw.individuals_alive_at(0):
+        if ts_Y_raw.individual(i).metadata["subpopulation"] == 1:
+            nodes_M_p1.extend(ts_Y_raw.individual(i).nodes)
+        elif ts_Y_raw.individual(i).metadata["subpopulation"] == 2:
+            nodes_M_p2.extend(ts_Y_raw.individual(i).nodes)
+        elif ts_Y_raw.individual(i).metadata["subpopulation"] == 3:
+            nodes_M_p3.extend(ts_Y_raw.individual(i).nodes)
         else:
             raise ValueError("Individual without population!")
     keep_nodes_M = np.concatenate((np.random.choice(nodes_M_p1, nSamples, replace=False), 
                                    np.random.choice(nodes_M_p2, nSamples, replace=False), 
                                    np.random.choice(nodes_M_p3, nSamples, replace=False)))
-    sts_YChrom = treeseq_YChrom.simplify(keep_nodes_M)
+    sts_Y = ts_Y_raw.simplify(keep_nodes_M)
 
-    mts_mtDNA = msprime.mutate(sts_mtDNA, rate=6.85*10**-7, random_seed=1, keep=False)
-    mts_YChrom = msprime.mutate(sts_YChrom, rate=3.01*10**-8, random_seed=1, keep=False)
+    mts_M = msprime.mutate(sts_M, rate=6.85*10**-7, random_seed=1, keep=False)
+    mts_Y = msprime.mutate(sts_Y, rate=3.01*10**-8, random_seed=1, keep=False)
 
-    mts_mtDNA = mts_mtDNA.delete_sites([i.id for i in mts_mtDNA.sites() if i.position < 900000])  
-    mts_YChrom = mts_YChrom.delete_sites([i.id for i in mts_YChrom.sites() if i.position >= 900000])
+    mts_M = mts_M.delete_sites([i.id for i in mts_M.sites() if i.position < 900000])  
+    mts_Y = mts_Y.delete_sites([i.id for i in mts_Y.sites() if i.position >= 900000])
     
-    mts_mtDNA.dump(os.path.join(ROOT_DIR, 'output', 'ts_parsed', inputFileName.replace('.trees', '_mtDNA.trees')))
-    mts_YChrom.dump(os.path.join(ROOT_DIR, 'output', 'ts_parsed', inputFileName.replace('.trees', '_YChrom.trees')))
+    mts_M.dump(os.path.join(ROOT_DIR, 'output', 'ts_parsed', inputFileName.replace('.trees', '_mtDNA.trees')))
+    mts_Y.dump(os.path.join(ROOT_DIR, 'output', 'ts_parsed', inputFileName.replace('.trees', '_YChrom.trees')))
     
     def convert_to_json_metadata(ts):
         # make a new ts with json metadata
@@ -126,43 +126,49 @@ def haplotypeCalc(path):
         # May also need to convert top level metadata?
         return new_tables.tree_sequence()
 
-    mtDNA_samplets = convert_to_json_metadata(mts_mtDNA)
-    YChrom_samplets = convert_to_json_metadata(mts_YChrom)
+    sample_ts_M = convert_to_json_metadata(mts_M)
+    sample_ts_Y = convert_to_json_metadata(mts_Y)
     
     with tsinfer.SampleData(
         path=os.path.join(ROOT_DIR, 'output', 'tsinfer_samples', inputFileName.replace('.trees', '_mtDNA.samples')), 
-        sequence_length=mtDNA_samplets.sequence_length, 
+        sequence_length=sample_ts_M.sequence_length, 
         num_flush_threads=2
-    ) as sample_data:
-        for var in mtDNA_samplets.variants():
-            sample_data.add_site(var.site.position, var.genotypes, var.alleles)
-    inferred_tree_mtDNA = tsinfer.infer(sample_data= sample_data)
+    ) as sample_data_M:
+        for var in sample_ts_M.variants():
+            sample_data_M.add_site(var.site.position, var.genotypes, var.alleles)
+    its_M = tsinfer.infer(sample_data= sample_data_M)
+    print(its_M.draw_text())
     
     with tsinfer.SampleData(
         path=os.path.join(ROOT_DIR, 'output', 'tsinfer_samples', inputFileName.replace('.trees', '_YChrom.samples')), 
-        sequence_length=YChrom_samplets.sequence_length, 
+        sequence_length=sample_ts_Y.sequence_length, 
         num_flush_threads=2
-    ) as sample_data:
-        for var in YChrom_samplets.variants():
-            sample_data.add_site(var.site.position, var.genotypes, var.alleles)
-    inferred_tree_YChrom = tsinfer.infer(sample_data= sample_data)
+    ) as sample_data_Y:
+        for var in sample_ts_Y.variants():
+            sample_data_Y.add_site(var.site.position, var.genotypes, var.alleles)
+    its_Y = tsinfer.infer(sample_data= sample_data_Y)
+    print(its_Y.draw_text())
     
-    inferred_tree_mtDNA.dump(os.path.join(ROOT_DIR, 'output', 'ts_inferred', inputFileName.replace('.trees', '_mtDNA.trees')))
-    inferred_tree_YChrom.dump(os.path.join(ROOT_DIR, 'output', 'ts_inferred', inputFileName.replace('.trees', '_YChrom.trees')))    
+    its_M.dump(os.path.join(ROOT_DIR, 'output', 'ts_inferred', inputFileName.replace('.trees', '_mtDNA.trees')))
+    its_Y.dump(os.path.join(ROOT_DIR, 'output', 'ts_inferred', inputFileName.replace('.trees', '_YChrom.trees')))
+    
+    sample_data_Y.close()
+    sample_data_M.close()
+    os.remove(os.path.join(ROOT_DIR, 'output', 'tsinfer_samples', inputFileName.replace('.trees', '_mtDNA.samples')))
+    os.remove(os.path.join(ROOT_DIR, 'output', 'tsinfer_samples', inputFileName.replace('.trees', '_YChrom.samples')))
+    
  
     output = open(os.path.join(ROOT_DIR, 'output', 'haplotypes', inputFileName.replace('.trees', '_mtDNA.txt')), "w")
-    for h,v in zip(mts_mtDNA.haplotypes(), mts_mtDNA.samples()):
-        pop=sts_mtDNA.individual(v).metadata["subpopulation"]
+    for h,v in zip(mts_M.haplotypes(), mts_M.samples()):
+        pop=sts_M.individual(v).metadata["subpopulation"]
         output.write(f"Pop{pop}_Ind{v}\t"+h+"\n")
     output.close()
 
     output = open(os.path.join(ROOT_DIR, 'output', 'haplotypes', inputFileName.replace('.trees', '_YChrom.txt')), "w")
-    for h,v in zip(mts_YChrom.haplotypes(), mts_YChrom.samples()):
-        pop=sts_YChrom.individual(v).metadata["subpopulation"]
+    for h,v in zip(mts_Y.haplotypes(), mts_Y.samples()):
+        pop=sts_Y.individual(v).metadata["subpopulation"]
         output.write(f"Pop{pop}_Ind{v}\t"+h+"\n")
     output.close()
     
     
-
-
 haplotypeCalc(path)
