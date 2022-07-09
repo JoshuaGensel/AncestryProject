90b9e3edb924219dd8e66e4f1d1d0c4bf9afc520
Leon Joshua Gensel <joshua.gensel@icloud.com>
2022-07-10 01:48:56 +0200

    tree_analysis proportion calculations

--- src/tree_analysis.py
+++ src/tree_analysis.py
@@ -1,9 +1,5 @@
-from fileinput import filename
-from tkinter import constants
-from tkinter.tix import Tree
-from unicodedata import name
+from msilib.schema import Error
 from Bio import Phylo
-from matplotlib.pyplot import draw, get
 import pandas as pd
 import os
 import ete3
@@ -22,10 +18,6 @@ ID = int(parameter_vector[3])
 test_phylo_tree = Phylo.read(os.path.join(ROOT_DIR, 'data', 'tree_genealogy', filename), "newick")
 test_ete_tree = ete3.Tree(os.path.join(ROOT_DIR, 'data', 'tree_genealogy', filename))
 
-P3_nodes = []
-for i in test_ete_tree.iter_leaves():
-    if i.name.startswith("P3"):
-        P3_nodes.append(i)
 
 def get_SP(node):
     sisternodes = set()
@@ -41,7 +33,53 @@ def get_SP(node):
     else:
         return get_SP(node.up)
 
+P3_nodes = []
+for i in test_ete_tree.iter_leaves():
+    if i.name.startswith("P3"):
+        P3_nodes.append(i)
+        
+N_SAMPLES = len(P3_nodes)
+
+p1_correct = 0
+p1_false = 0
+p2_correct = 0
+p2_false = 0
+unknown_SP1 = 0
+unknown_SP2 = 0
+
+
 for i in P3_nodes:
     print(i.name, ": ", get_SP(i))
+    if get_SP(i) == "SP1":
+        if i.name[-3:] == get_SP(i):
+            p1_correct += 1
+        else:
+            p1_false += 1
+    elif get_SP(i) == "SP2":
+        if i.name[-3:] == get_SP(i):
+            p2_correct += 1
+        else:
+            p2_false += 1
+    elif get_SP(i) == "unknown":
+        if i.name[-3:] == "SP1":
+            unknown_SP1 += 1
+        else:
+            unknown_SP2 += 1
+    else:
+        raise Error("Nothing inferred!")
+    
 
-Phylo.draw(test_phylo_tree)
+p1_correct = p1_correct/N_SAMPLES
+p1_false = p1_false/N_SAMPLES
+unknown_SP1 = unknown_SP1/N_SAMPLES
+p2_correct = p2_correct/N_SAMPLES
+p2_false = p2_false/N_SAMPLES
+unknown_SP2 = unknown_SP2/N_SAMPLES
+
+p1_proportion = p1_correct + p1_false
+p2_proportion = p2_correct + p2_false
+unknown_proportion = unknown_SP1 + unknown_SP2
+false_proportion = p1_false + p2_false
+
+print(false_proportion, unknown_proportion)
+#Phylo.draw(test_phylo_tree)
